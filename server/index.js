9dfed06774f07f930840c124addc2cda56c76baa
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-11-17 14:49:07 +0530

    added get request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import Link from './models/Link.js';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,53 @@ const connectDB = async () =>{
     }
 };
 connectDB();
+
+app.post("/link", async (req, res)=>{
+    const {url, slug} = req.body;
+
+    const randomSlug = Math.random().toString(36).substring(2,7);
+    const link = new Link({
+        url: url,
+        slug: slug || randomSlug
+    })
+
+    try{
+        const savedLink = await link.save();
+         return res.json({
+            success: true,
+            data:{
+              
+               shortLink:`${process.env.BASE_URL}/${savedLink.slug}` 
+            },
+            message: "Link saved successfully"
+        })
+    }
+    catch(err){
+        return res.json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+app.get("/:slug", async (req, res)=>{
+    const {slug} = req.params;
+
+    const link = await Link.findOne({slug: slug});
+
+    await Link.updateOne({slug: slug}, {$set: {
+        clicks: link.clicks + 1
+     }})
+
+    if(!link){
+        return res.json({
+            success: false,
+            message: "Link not found"
+        })
+    }
+   
+    res.redirect(link.url);
+})
 
 const PORT = process.env.PORT || 5000;
 
